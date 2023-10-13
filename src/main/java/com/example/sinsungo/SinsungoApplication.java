ad88046140dc88076e3b71e43cc4b6065ab26a93
SeungBum Baek <wer06099@naver.com>
2023-10-13 13:28:26 +0900

    초기세팅

---
+++ src/main/java/com/example/sinsungo/SinsungoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sinsungo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsungoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinsungoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sinsungo/SinsungoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sinsungo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinsungoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
