509e70458a063fe2bacf98135293d790a4c99f9b
seulgi <asd0927122@gmail.com>
2023-10-31 20:40:29 +0900

    Add(SG) : 냉장고 재료 CRUD & 재료 관려 Entity 작성 및 연관관계 #5

--- src/main/java/com/example/sinsungo/ingredient/RefrigeratorIngredient/RefrigeratorIngredient.java
+++ src/main/java/com/example/sinsungo/ingredient/RefrigeratorIngredient/RefrigeratorIngredient.java
@@ -1,14 +1,19 @@
 package com.example.sinsungo.ingredient.RefrigeratorIngredient;
 
+import com.example.sinsungo.ingredient.RefrigeratorIngredient.dto.RefrigeratorIngredientRequestDto;
 import com.example.sinsungo.ingredient.entity.Ingredient;
+import com.example.sinsungo.ingredient.entity.IngredientUnitEnum;
 import com.example.sinsungo.refrigerator.Refrigerator;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
+import com.example.sinsungo.user.User;
+
 
 @Entity
 @Getter
@@ -16,11 +21,42 @@ import org.hibernate.annotations.DynamicUpdate;
 @NoArgsConstructor
 @Table(name = "refrigerator_ingredients")
 public class RefrigeratorIngredient extends Ingredient {
+
     private String category;
 
-    private String deadline;
+    private LocalDateTime deadline;
 
-    @ManyToOne
-    @JoinColumn(name = "refrigerator_id")
-    private Refrigerator refrigerator;
+    public RefrigeratorIngredient(RefrigeratorIngredientRequestDto requestDto, User user, Refrigerator refrigerator) {
+        this.category = requestDto.getCategory();
+        this.deadline = requestDto.getDeadline();
+        super.name = requestDto.getName();
+        super.quantity = requestDto.getQuantity();
+        super.unit = requestDto.getUnit();
+        super.user = user;
+        super.refrigerator = refrigerator;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setName(String name) {
+        super.name = name;
+    }
+
+    public void setQuantity(Long quantity) {
+        super.quantity = quantity;
+    }
+
+    public void setUnit(IngredientUnitEnum unit) {
+        super.unit = unit;
+    }
+
+    public void setRefrigerator(Refrigerator refrigerator) {
+        super.refrigerator = refrigerator;
+    }
 }
